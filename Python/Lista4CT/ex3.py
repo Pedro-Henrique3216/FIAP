2d5858f5be9aa648c5ae71fba5680b063240fecd
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-04-22 11:13:05 -0300

    refatorando ex3

--- Python/Lista4CT/ex3.py
+++ Python/Lista4CT/ex3.py
@@ -1,11 +1,15 @@
 # trocando letras das palavras sem usar o replace
-frase = input("Escreva uma frase: ").lower()
+frase = input("Escreva uma frase: ")
 
 letra = input("Digite uma letra: ").lower()
+while len(letra) != 1:
+    letra = input("Erro digite uma letra: ").lower()
 
 outher_frase = ""
+
+letra_M = letra.upper()
 for i in frase:
-    if(i == letra):
+    if(i == letra or i == letra_M):
         outher_frase += "*"
     else:
         outher_frase += i
