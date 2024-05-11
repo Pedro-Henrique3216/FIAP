76c61c4b108f9343b5217cc4cdb27b74ad820cf5
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-11 14:35:14 -0300

    fix: add variable to get today plus 3 and add variable to get today minus 3

--- JavaMaven/simuladoCP2/src/test/java/org/example/CalculaIntervaloEmDiasTest.java
+++ JavaMaven/simuladoCP2/src/test/java/org/example/CalculaIntervaloEmDiasTest.java
@@ -4,15 +4,22 @@ import org.example.ex3.CalculaIntervaloEmDias;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAmount;
+
 class CalculaIntervaloEmDiasTest {
 
     @Test
-    void se_dia_igual_a_11_5_2024_diferenca_igual_3() {
-        Assertions.assertEquals(3, CalculaIntervaloEmDias.calculaIntevalo("11/05/2024"));
+    void se_dia_igual_a_hoje_mais_3_diferenca_igual_3() {
+        String diaHojeMais3 = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Assertions.assertEquals(3, CalculaIntervaloEmDias.calculaIntevalo(diaHojeMais3));
     }
 
     @Test
-    void se_dia_igual_a_03_5_2024_diferenca_igual_2() {
-        Assertions.assertEquals(2, CalculaIntervaloEmDias.calculaIntevalo("06/05/2024"));
+    void se_dia_igual_menos_2_diferenca_igual_2() {
+        String diaHojeMenos2 = LocalDate.now().minusDays(2).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Assertions.assertEquals(2, CalculaIntervaloEmDias.calculaIntevalo(diaHojeMenos2));
     }
 }
