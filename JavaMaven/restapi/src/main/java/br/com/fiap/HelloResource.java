651145b410299ac355d994371ad6965c5ad59c2f
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-01 20:57:10 -0300

    exemplo de como liberar api

--- JavaMaven/restapi/src/main/java/br/com/fiap/HelloResource.java
+++ JavaMaven/restapi/src/main/java/br/com/fiap/HelloResource.java
@@ -1,2 +1,22 @@
-package br.com.fiap;public class HelloResource {
+package br.com.fiap;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/demo")
+public class HelloResource {
+
+    @GET
+    @Path("/v1")
+    public String sayHello(){
+        return """
+                {"mensagem": "Hello World"}
+                """;
+    }
+
+    @GET
+    @Path("/v2")
+    public String sayHelloVersao2(){
+        return "{\"mensagem\":\"Hello, World\"}";
+    }
 }
