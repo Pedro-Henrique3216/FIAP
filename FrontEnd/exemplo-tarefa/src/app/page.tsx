1a505c8226c2a4afd69953956783bb6c56a29e29
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-30 22:29:39 -0300

    exemplo de funções com next

---
+++ FrontEnd/exemplo-tarefa/src/app/page.tsx
@@ -0,0 +1,10 @@
+import ListaTarefas from "@/components/ListaTarefas";
+
+export default function Home() {
+  return (
+    <div>
+       <h1>Exemplo Tarefa</h1>
+       <ListaTarefas />
+    </div>
+  );
+}
