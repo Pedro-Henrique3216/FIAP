b4899dfa5c4d4f7db48e9fb0e92b7a5b7a1d4b52
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-08 09:48:22 -0300

    usando metodos de post e put para add pessoa a um map

---
+++ JavaMaven/restapi/src/main/java/br/com/fiap/FakeDb.java
@@ -0,0 +1,9 @@
+package br.com.fiap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FakeDb {
+
+    public static Map<Long, Pessoa> pessoa = new HashMap<>();
+}
--- JavaMaven/restapi/src/main/java/br/com/fiap/HelloResource.java
+++ JavaMaven/restapi/src/main/java/br/com/fiap/HelloResource.java
@@ -1,13 +1,16 @@
 package br.com.fiap;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
 
 @Path("/demo")
 public class HelloResource {
 
     @GET
     @Path("/v1")
+    @Produces(MediaType.APPLICATION_JSON)
     public String sayHello(){
         return """
                 {"mensagem": "Hello World"}
@@ -16,7 +19,39 @@ public class HelloResource {
 
     @GET
     @Path("/v2")
+    @Produces(MediaType.APPLICATION_JSON)
     public String sayHelloVersao2(){
         return "{\"mensagem\":\"Hello, World\"}";
+    }
+
+    @GET
+    @Path("/pessoas")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response buscarPessoa(){
+        return Response.status(Response.Status.OK).entity(FakeDb.pessoa).build();
+    }
+
+    @POST
+    @Path("/pessoa")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response inserirPessoa(Pessoa pessoa){
+        if (pessoa.getId() == null){
+            return Response.status(Response.Status.BAD_REQUEST).entity(Map.of("Mensagem", "id invalido")).build();
+        }
+        FakeDb.pessoa.put(pessoa.getId(), pessoa);
+        return Response.status(Response.Status.CREATED).entity(pessoa).build();
+    }
+
+    @PUT
+    @Path("/pessoa/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") Long id, Pessoa pessoa){
+        if (!FakeDb.pessoa.containsKey(id)){
+            return Response.status(Response.Status.NOT_FOUND).entity(Map.of("Mensagem", "id invalido")).build();
+        }
+        FakeDb.pessoa.replace(id, pessoa);
+        return Response.status(Response.Status.CREATED).entity(pessoa).build();
     }
 }
