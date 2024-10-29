dd9c2a066e7eb02e36b8e8e546ef6000191bf30f
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-29 11:02:01 -0300

    initial commit next-context

---
+++ FrontEnd/next-context/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex p-10 justify-center">
+        <h1 className="text-3xl font-bold text-indigo-800">Home</h1>
+    </main>
+  );
+}
