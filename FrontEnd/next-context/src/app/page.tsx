4aa407f1a28d510cd78350530637e5f2d39a11bc
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-05-05 08:44:56 -0300

    add context

--- FrontEnd/next-context/src/app/page.tsx
+++ FrontEnd/next-context/src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+import { AuthContext } from "@/context";
+import { useContext } from "react";
+
 export default function Home() {
+
+  const {user, login, logout} = useContext(AuthContext)
   return (
     <main className="flex p-10 justify-center">
         <h1 className="text-3xl font-bold text-indigo-800">Home</h1>
+        <p>Nome do usuario: {user?.nome}</p>
     </main>
   );
 }
